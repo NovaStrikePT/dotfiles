4f5928e6cc3861b14f4164b3669cc78ba8ee697a
Phuong Tran <NovaStrikePT@users.noreply.github.com>
2015-03-31 16:24:20 -0500

    Adding aliases to config.fish

--- config.fish
+++ config.fish
@@ -8,6 +8,25 @@ set -g -x PATH {$HOME}/bin $PATH
 set -g -x fish_greeting ''
 
 # "Aliases"
-function lsa
-	ls -Glah $argv
+#
+alias lsa 'ls -Glah'
+
+# Make vi always run vim
+alias vi 'vim'
+
+# grep in color
+alias grep 'grep --color=auto'
+
+# Brew Cask
+alias cask 'brew cask'
+
+# Find with extended regex
+alias efind 'find -E'
+
+# Remove newline at end of file - for those super minor changes where vi still makes a needless modification
+alias chompeofnewline "perl -pi -e 'chomp if eof'"
+
+# Show all file extensions for files in the current directory hierarchy
+function fileextensions
+	find . -type f | perl -ne 'print $1 if m/\.([^.\/]+)$/' | sort -u
 end
