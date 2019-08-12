192c43f2d177a6c4b118d5a21a36ad00a28b451b
Phuong Tran <NovaStrikePT@users.noreply.github.com>
2019-08-12 15:51:37 -0500

    Adding PSReadLine chord mappings for VS Code

--- modes/win10/bin/Windows/profiles/AllUsersAllHosts.ps1
+++ modes/win10/bin/Windows/profiles/AllUsersAllHosts.ps1
@@ -1,6 +1,15 @@
 # Disable term bell
 Set-PSReadlineOption -BellStyle None
 
+# Map PSReadLine key handlers using chords VSCode understands in its integrated terminals so that default keys work
+# like the external terminal (e.g. Ctrl+Backspace, Ctrl+Del, Ctrl+Space)
+# See also PowerShell vscode-powershell issue 535
+if ($env:TERM_PROGRAM -eq "vscode") {
+    Set-PSReadLineKeyHandler -Chord 'Ctrl+w' -Function BackwardKillWord
+    Set-PSReadLineKeyHandler -Chord 'Alt+D' -Function KillWord
+    Set-PSReadLineKeyHandler -Chord 'Ctrl+@' -Function MenuComplete
+}
+
 # `rg` aliases
 function arg {
     rg.exe --hidden --pretty --smart-case --no-ignore-vcs --glob !.git $args
