c01613923d9b95f3e7e57b517633060c924ba3c3
Phuong Tran <NovaStrikePT@users.noreply.github.com>
2019-09-12 20:37:09 -0500

    Removing no-longer used configs (Mac, bash)

--- modes/win10/bin/Windows/profiles/AllUsersAllHosts.ps1
+++ modes/win10/bin/Windows/profiles/AllUsersAllHosts.ps1
@@ -15,12 +15,6 @@ function arg {
     rg.exe --hidden --pretty --smart-case --no-ignore-vcs --glob !.git $args
 }
 
-# Use a specialized profile to start Git bash for Windows
-# See git-bash-for-windows.profile
-function git-bash {
-    & $env:ProgramFiles\Git\bin\bash.exe --init-file $HOME\git-bash-for-windows.profile $args
-}
-
 # Confer with `Find-Module -Name Recycle`
 Set-Alias -Name recycle -Value Remove-ItemSafely
 
