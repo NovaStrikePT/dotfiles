31c0b69caf971ee421ce679b64be1c4eb91692a1
Phuong Tran <NovaStrikePT@users.noreply.github.com>
2019-09-12 20:38:01 -0500

    Adding FileSize format PowerShell function

--- modes/win10/bin/Windows/profiles/AllUsersAllHosts.ps1
+++ modes/win10/bin/Windows/profiles/AllUsersAllHosts.ps1
@@ -15,10 +15,23 @@ function arg {
     rg.exe --hidden --pretty --smart-case --no-ignore-vcs --glob !.git $args
 }
 
+# Human-readable file sizes
+# Source: https://superuser.com/questions/468782/show-human-readable-file-sizes-in-the-default-powershell-ls-command#answer-468795
+# Example usage: Get-ChildItem | Select-Object Name, @{Name="Size";Expression={Format-FileSize($_.Length)}}
+Function Format-FileSize() {
+    Param ([uint64]$size)
+    If     ($size -gt 1TB) {[string]::Format("{0:0.00} TB", $size / 1TB)}
+    ElseIf ($size -gt 1GB) {[string]::Format("{0:0.00} GB", $size / 1GB)}
+    ElseIf ($size -gt 1MB) {[string]::Format("{0:0.00} MB", $size / 1MB)}
+    ElseIf ($size -gt 1KB) {[string]::Format("{0:0.00} kB", $size / 1KB)}
+    ElseIf ($size -gt 0)   {[string]::Format("{0:0.00} B" , $size)}
+    Else                   {""}
+}
+
 # Confer with `Find-Module -Name Recycle`
 Set-Alias -Name recycle -Value Remove-ItemSafely
 
 # Last bootup time
-function Last-Bootup-Time {
+function Get-LastBootupTime {
     return (Get-CimInstance Win32_OperatingSystem).LastBootUpTime
 }
