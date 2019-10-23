df1a70eb66a015735a885e8c7b711927fa1fe4b0
Phuong Tran <NovaStrikePT@users.noreply.github.com>
2019-10-23 10:11:06 -0500

    Making size param mandatory and able to accept pipeline input

--- modes/win10/bin/Windows/profiles/AllUsersAllHosts.ps1
+++ modes/win10/bin/Windows/profiles/AllUsersAllHosts.ps1
@@ -22,7 +22,11 @@ function arg
 # Example usage: Get-ChildItem | Select-Object Name, @{Name="Size";Expression={Format-FileSize($_.Length)}}
 function Format-FileSize
 {
-    Param ([uint64]$size)
+    Param(
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [uint64]
+        $size
+    )
     If     ($size -gt 1TB) {[string]::Format("{0:0.00} TB", $size / 1TB)}
     ElseIf ($size -gt 1GB) {[string]::Format("{0:0.00} GB", $size / 1GB)}
     ElseIf ($size -gt 1MB) {[string]::Format("{0:0.00} MB", $size / 1MB)}
