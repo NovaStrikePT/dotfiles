5a94475c0e4f61143712710649bed24c9e906e16
Phuong Tran <NovaStrikePT@users.noreply.github.com>
2023-01-25 08:56:23 -0600

    Cleanup: removing unused aliases and configuration variables. Also changing a few other aliases to abbreviations

--- modes/win10/.config/fish/config.fish
+++ modes/win10/.config/fish/config.fish
@@ -9,77 +9,39 @@ set -gx fish_greeting ''
 set -gx LESS '--RAW-CONTROL-CHARS --quit-if-one-screen --no-init' # Alternatively, `-RFX`
 set -gx LESSOPEN "| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"
 
-# "Aliases"
-#
-alias lsa 'ls -lah --color=auto'
-
-# Single-depth du
-alias du-hd1 'du -h --max-depth=1'
-
 # Use vim as default editor
 set -gx VISUAL 'vim'
 set -gx EDITOR "$VISUAL"
-alias vi 'vim'
 
-# vdr using tabpages
-alias viet 'vdr -t'
+#region Aliases/abbreviations
 
-# grep in color
-alias grep 'grep --color=auto'
+abbr lsa --add ls -lah --color=auto
+
+# Single-depth du
+abbr --add du1 du -h --max-depth=1
 
 # Use colordiff whenever possible
 if type -q colordiff 2>/dev/null;
 	alias diff 'colordiff'
 end
 
-# Find with extended regex
-alias efind 'find -regextype posix-extended'
-
-# Get formatted mtime of a file
-alias mtime 'stat -f "%Sm" -t "%Y-%m-%d %H.%M"'
-
-# WSL interacting with Windows: set TMPDIR to a Windows-accessible location
-# This is primarily used only for git diff/merge GUI tools
-# See https://www.sep.com/sep-blog/2017/06/07/20170607wsl-git-and-beyond-compare/
-alias gitwsl "env TMPDIR=$HOME/AppData/Local/Temp/ git"
-
-# Remove newline at end of file - for those super minor changes where vi still makes a needless modification
-alias chompeofnewline "perl -pi -e 'chomp if eof'"
+# `rg` commonly used preset
+function arg
+	# See also $LESS options above
+	rg --hidden --pretty --smart-case --no-ignore-vcs --glob \!.git $argv | less
+end
 
 # Show all file extensions for files in the current directory hierarchy
 function fileextensions
 	fd --type file --no-ignore-vcs | awk --field-separator . '{print $NF}' | sort | uniq --count | awk '{print $2,$1}'
 end
 
-# Common rsync over ssh macro
-alias rsshnc 'rsync -avh --progress -e ssh'
+#endregion
 
-# FZF configuration
-#
-
+# Use this file-discovery command for `fzf` by default
 set -gx FZF_DEFAULT_COMMAND 'fd --type file --hidden --no-ignore-vcs --exclude .git'
 
-# Use 16-color terminal colors by default
-set -gx FZF_DEFAULT_OPTS "--color=16"
-
-# Search everything for command-line pasting (CTRL+T)
-set -gx FZF_CTRL_T_COMMAND 'fd --hidden --no-ignore-vcs'
-
-# Search all directories for `cd`-ing to (ALT+C)
-# Provide a `tree` preview
-set -gx FZF_ALT_C_COMMAND 'fd --type directory --hidden --no-ignore-vcs'
-set -gx FZF_ALT_C_OPTS "--preview 'tree -C {} | head -100'"
-
-# Use `?` to toggle preview window for history commands (useful for longer commands)
-set -gx FZF_CTRL_R_OPTS "--preview 'echo {}' --preview-window down:3:hidden:wrap --bind '?:toggle-preview'"
-
-# `rg` aliases
-function arg
-	# See also $LESS options above
-	rg --hidden --pretty --smart-case --no-ignore-vcs --glob \!.git $argv | less
-end
-
-# Source (optional) local configuration
+# Source additional local configuration file, if it exists
 set -l local_config {$HOME}/.config/fish/local.fish
 if test -f $local_config
 	source $local_config
